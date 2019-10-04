e5f86f16c73982813a0349ae3341b6f887510d2a
Eyda <cjangie13@gmail.com>
2019-10-03 21:53:36 -0400

    clear the guesses and done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,42 @@
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let wins=0;
+let wins = 0;
 let loses = 0;
-let left=9;
-let arrayg=[];
-let count=0;
-let emptyarray=[];
-document.onkeyup=function(){
+let left = 9;
+let arrayg = [];
+let count = 0;
 
-   let pcguess =letters[Math.floor(Math.random() * letters.length)];
-   let userguess= String.fromCharCode(event.keyCode).toLowerCase();
-   if(left==0){
-loses++;
-document.getElementById('loses').innerHTML=loses;
-alert("u lost, the pc guess was: "+pcguess);
-left=9;
-document.getElementById('left').innerHTML=left;
 
-   }else if(left!=0){
-       if(pcguess!=userguess){
-           left--;
-           document.getElementById('left').innerHTML=left;
-        arrayg[count]=userguess;
-        document.getElementById('arrayg').innerHTML=arrayg;
-        count++;
-       }else if(pcguess==userguess){
-           wins++;
-           document.getElementById('wins').innerHTML=wins;
-           alert("u won the pc guess was: "+pcguess);
-           left=9;
-           document.getElementById('left').innerHTML=left;
-       }
-   }
+let pcguess = letters[Math.floor(Math.random() * letters.length)];
+document.onkeyup = function () {
+
+
+    let userguess = String.fromCharCode(event.keyCode).toLowerCase();
+    if (left == 0) {
+        loses++;
+        document.getElementById('loses').innerHTML = loses;
+        alert("u lost, the pc guess was: " + pcguess);
+        left = 9;
+        document.getElementById('left').innerHTML = left;
+        pcguess = letters[Math.floor(Math.random() * letters.length)];
+        // here should i clear the guesses
+    } else {
+        if (pcguess != userguess) {
+            left--;
+            document.getElementById('left').innerHTML = left;
+            arrayg[count] = userguess;
+            document.getElementById('arrayg').innerHTML = arrayg;
+            count++;
+        } else {
+            wins++;
+            document.getElementById('wins').innerHTML = wins;
+            alert("u won the pc guess was: " + pcguess);
+            left = 9;
+            document.getElementById('left').innerHTML = left;
+            pcguess = letters[Math.floor(Math.random() * letters.length)];
+            // here should i clear the guesses
+
+        }
+    }
 
 }
 
